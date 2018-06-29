7ac0451a12848b1531fd73cde085cf7a5b8a12f4
Jakub Stefan <kejkun1992@gmail.com>
2018-06-29 15:18:40 +0200

    addition of a replacement function

--- todo_in_console/todo.py
+++ todo_in_console/todo.py
@@ -22,7 +22,7 @@ def adding(new_task):
 
 def deleting(id_num):
     with open('todo_tasks.txt') as tasks_list:
-        tasks = [line.split() for line in tasks_list]
+        tasks = [line.split('\t') for line in tasks_list]
     num_list = [item[0] for item in tasks]
     if id_num in num_list and id_num != 'ID':
         new_list = [line for line in tasks if line[0] != id_num]
@@ -33,23 +33,45 @@ def deleting(id_num):
                 n += 1
         with open('todo_tasks.txt', 'w') as tasks_list:
              for line in new_list:
-                 print('\t'.join(line), file=tasks_list)
+                 print('\t'.join(line), file=tasks_list, end='')
     else:
         print('There is no task for such an ID.')
-        
+
+
+def replacing(id_num, new_task):
+    with open('todo_tasks.txt') as tasks_list:
+        tasks = [line.split('\t') for line in tasks_list]
+    num_list = [item[0] for item in tasks]
+    if id_num in num_list and id_num != 'ID':
+        new_tasks = []
+        for line in tasks:
+            if id_num == line[0]:
+                line = [line[0]] + [new_task + '\n']
+                new_tasks.append(line)
+            else:
+                new_tasks.append(line)
+        with open('todo_tasks.txt', 'w') as tasks_list:
+            for line in new_tasks:
+                 print('\t'.join(line), file=tasks_list, end='')
+    else:
+        print('There is no task for such an ID.')
         
 def main():
     while True:
         display_list()
-        what_todo = input('Add(A) or delete(D) a task?\n')
+        what_todo = input('Add(A), replace(R) or delete(D) a task?\n')
         if what_todo == 'A':
             new_task = input('Enter the task:\n')
             adding(new_task)
         elif what_todo == 'D':
             id_num = input('Enter the ID of the task to be deleted:\n')
             deleting(id_num)
+        elif what_todo == 'R':
+            id_num = input('Enter the ID of the task to be replaced:\n')
+            new_task = input('Enter the task:\n')
+            replacing(id_num, new_task)
         else:
             main()
 
-print('To do list\n\n')
+print('To-do list\n\n')
 main()
