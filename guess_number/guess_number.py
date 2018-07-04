d2a229766d7146185ac334758a32e610bb0e252d
Jakub Stefan <kejkun1992@gmail.com>
2018-07-04 14:09:16 +0200

    guess number

--- guess_number/guess_number.py
+++ guess_number/guess_number.py
@@ -7,18 +7,30 @@ def random_num():
 
 
 def checking_num(num, players_num, tries_num):
-    if num == players_num:
+    if players_num > 100 or players_num < 1:
+        print('Wrong number!')
+    elif num == players_num:
         print('Win')
+        print('Number of tries: %s' % tries_num)
         main()
     elif num > players_num:
         print('Give a larger number')
-    elif num > players_num:
+    elif num < players_num:
         print('Give a smaller number')
 
 
 def main():
     tries_num = 0
     num = random_num()
-    print('')
+    print('Guess the number from 1 to 100')
     while 1:
-        pass
+        players_num = input('')
+        try:
+            tries_num += 1
+            players_num = int(players_num)
+            checking_num(num, players_num, tries_num)
+            print('Try again!')  
+        except ValueError:
+            print('This is not a number!')
+            
+main()
