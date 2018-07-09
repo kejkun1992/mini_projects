288c947124b06421e8992b9a94370e95b2d893ef
Jakub Stefan <kejkun1992@gmail.com>
2018-07-09 16:52:19 +0200

    battleships root

--- battleships_in_console/battleships.py
+++ battleships_in_console/battleships.py
@@ -1,2 +1,48 @@
 # -*- coding: utf-8 -*-
 
+import random
+
+
+class Computer:
+    def __init__(self):
+        pass
+    
+    def create_battlefiled(self):
+        pass
+    
+    def attack(self):
+        pass
+    
+    def defence(self):
+        pass
+    
+    def display(self):
+        pass
+    
+    
+class Player:
+    def __init__(self):
+        pass
+    
+    def create_battlefiled(self):
+        pass
+    
+    def attack(self):
+        pass
+    
+    def defence(self):
+        pass
+    
+    def display(self):
+        pass
+    
+    
+class Mainloop:
+    def __init__(self, computer, player):
+        self.computer = computer
+        self.player = player
+        
+    def game_loop(self):
+        pass
+        
+mainloop = Mainloop(Computer(), Player())
