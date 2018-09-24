0a138f9227418e505e08c7da99308ff695ab7211
Jakub Stefan <kejkun1992@gmail.com>
2018-09-24 19:03:04 +0200

    new way of recording

--- multiplication_in_memory/multi.py
+++ multiplication_in_memory/multi.py
@@ -3,31 +3,12 @@
 import sys, random, time
 
 
-def number_of_digits():
-    # player chooses number of digits in each of multiplied numbers
-    num = input('Test start. Choose number of digits in each of multiplied numbers(1-10): ')
-    try:
-        if int(num) > 10 or int(num) < 1:
-            print('Wrong value. Bye')
-            sys.exit()
-        return int(num)
-    except ValueError:
-        print('Wrong value. Bye')
-        sys.exit()
+def numbers_range():
+    # player chooses range of numbers to multiply
+    num = input('Test start. Choose range of numbers to multiply(e.g.: 1-10): ')
+    num = num.split('-')
+    return list(map(int, num))
 
-
-def number_of_numbers():
-    # player chooses number of numbers to multiply
-    num = input('Choose number of numbers to multiply(2-5): ')
-    try:
-        if int(num) > 5 or int(num) < 2:
-            print('Wrong value. Bye')
-            sys.exit()
-        return int(num)
-    except ValueError:
-        print('Wrong value. Bye')
-        sys.exit()
-        
       
 def list_to_str(nums: list) -> str:
     # changes list of numbers to operation in string
@@ -35,43 +16,31 @@ def list_to_str(nums: list) -> str:
     return '*'.join(nums)
     
     
-def random_operation(num_of_digits: int, num_of_nums: int) -> str:
+def random_operation(num1: int, num2: int) -> str:
     # creats random operation to solve
     nums = []
-    for i in range(num_of_nums):
-        if num_of_digits == 1:
-            nums.append(random.randint(1, 9))
-        elif num_of_digits == 2:
-            nums.append(random.randint(10, 99))
-        elif num_of_digits == 3:
-            nums.append(random.randint(100, 999))
-        elif num_of_digits == 4:
-            nums.append(random.randint(1000, 9999))
-        elif num_of_digits == 5:
-            nums.append(random.randint(10000, 99999))
+    for i in range(2):
+        nums.append(random.randint(num1, num2))
     return list_to_str(nums)
     
 
 def main():
-    num_of_digits = number_of_digits()
-    num_of_nums = number_of_numbers()
-    for i in range(3):
-        operation = random_operation(num_of_digits, num_of_nums)
+    numbersRange = numbers_range()
+    list_of_time = []
+    for i in range(20):
+        operation = random_operation(numbersRange[0], numbersRange[1])
         t1 = time.time()
         result = input(operation + ' = ')
-        try:
-            if int(result) == eval(operation):
-                # saves user's time if result is good
-                t2 = time.time()
-                t = t2 - t1
-                with open('results\\results.txt', 'at') as results:
-                    print(round(t, 1), file=results)
-            else:
-                print('Wrong answer')
-        except ValueError:
-            print('Wrong value. Bye')
-            sys.exit()
-        
+        if int(result) == eval(operation):
+            # saves user's time if result is good
+            t2 = time.time()
+            t = t2 - t1
+            list_of_time.append(round(t, 2))
+        else:
+            print('Wrong answer')
+    print(round(sum(list_of_time)/len(list_of_time), 2))
+    with open('results\\results.txt', 'at') as results:
+        print(str(numbersRange[0])+'-'+str(numbersRange[1])+'\t'+str(len(list_of_time))+'\t'+str(round(sum(list_of_time)/len(list_of_time),2)), file=results)
     
 if __name__ == '__main__':        
     main()
--- multiplication_in_memory/test_multi.py
+++ multiplication_in_memory/test_multi.py
@@ -8,11 +8,5 @@ class TestMulti(unittest.TestCase):
     def test_list_to_str(self):
         self.assertEqual(multi.list_to_str([1, 2, 3]), '1*2*3')
         
-    def test_random_operation1(self):
-        self.assertEqual(len(multi.random_operation(1, 3)), 5)
-        
-    def test_random_operation2(self):
-        self.assertEqual(len(multi.random_operation(3, 4)), 15)
-        
 if __name__ == '__main__':
     unittest.main()
